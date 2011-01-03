3f051e6b5de988a4437fe1be80495c5f06fe4aa1
Regis Pires Magalhaes <regispiresmag@gmail.com>
2011-01-03 11:53:39 -0300

    updated win and linux scripts and added windows binaries

--- build-easy-rails.sh
+++ build-easy-rails.sh
@@ -1,45 +1,128 @@
 #!/bin/bash
+VERSION=0.9.6
+RUBY_VERSION=1.8.7-p302
+RUBYGEMS_VERSION=1.3.7
+SQLITE_VERSION=3070400
+RAILS_VERSION=2.3.10
+
 SOURCE_DIR="$PWD"
-TARGET_DIR=/opt/easy-rails
-SQLITE=3.6.16
-RUBYGEMS=rubygems-1.3.5
-RUBY=ruby-1.8.7-p174
+TARGET_DIR="$HOME/easy-rails"
+DOWNLOAD_DIR="$SOURCE_DIR/downloads"
+
+PATH="$TARGET_DIR/ruby/bin":"$PATH"
+GEM_HOME="$TARGET_DIR/ruby/lib/ruby/gems/1.8"
+RUBY_HOME="$TARGET_DIR/ruby"
+export RUBYLIB="$TARGET_DIR/ruby/lib"
+RUBYOPT=
 
 # Creating directories
-sudo mkdir "$TARGET_DIR"
-sudo mkdir "$TARGET_DIR/bin"
-sudo mkdir "$TARGET_DIR/sqlite"
-sudo mkdir "$TARGET_DIR/sqlite/libs"
-sudo mkdir "$TARGET_DIR/rails_apps"
-sudo chmod a+w "$TARGET_DIR/rails_apps"
+mkdir -p "$DOWNLOAD_DIR"
+mkdir -p "$TARGET_DIR/bin"
+mkdir -p "$TARGET_DIR/ruby"
+mkdir -p "$TARGET_DIR/sqlite"
+mkdir -p "$TARGET_DIR/rails_apps"
+chmod a+w "$TARGET_DIR/rails_apps"
 
-# Copying files
-sudo cp linux/initialize.sh "$RUBYGEMS.zip" "$TARGET_DIR/bin/"
-sudo cp linux/ruby_console.sh linux/*.txt "$TARGET_DIR"
-sudo cp -r extra/* "$TARGET_DIR"
-sudo cp linux/sqlite*.bin.gz "$TARGET_DIR/sqlite/"
-sudo cp linux/sqlite*.so.gz "$TARGET_DIR/sqlite/libs/"
+# Downloading files
+echo Downloading files...
+if [ ! -f "$DOWNLOAD_DIR/ruby-$RUBY_VERSION.tar.gz" ]
+then
+	wget ftp://ftp.ruby-lang.org/pub/ruby/ruby-$RUBY_VERSION.tar.gz -P "$DOWNLOAD_DIR"
+fi
 
-# Uncompressing SQLite
-sudo gunzip -f "$TARGET_DIR/sqlite/sqlite3-$SQLITE.bin.gz"
-sudo gunzip -f "$TARGET_DIR/sqlite/libs/sqlite-$SQLITE.so.gz"
+if [ ! -f "$DOWNLOAD_DIR/rubygems-$RUBYGEMS_VERSION.zip" ]
+then
+	wget http://production.cf.rubygems.org/rubygems/rubygems-$RUBYGEMS_VERSION.zip -P "$DOWNLOAD_DIR"
+fi
 
-# Creating SQLite symbolic links
-cd "$TARGET_DIR/sqlite"
-sudo ln -s "sqlite3-$SQLITE.bin" sqlite3
-cd "$TARGET_DIR/sqlite/libs"
-sudo ln -s sqlite-$SQLITE.so libsqlite3.so
+if [ ! -f "$DOWNLOAD_DIR/sqlite-shell-linux-x86-$SQLITE_VERSION.zip" ]
+then
+	wget http://www.sqlite.org/sqlite-shell-linux-x86-$SQLITE_VERSION.zip -P "$DOWNLOAD_DIR"
+fi
+echo -e "Files downloaded successfully.\n"
+
+echo Copying Easy-Rails files...
+echo $VERSION > "$TARGET_DIR/version.txt"
+cp "$SOURCE_DIR"/linux/*.* "$TARGET_DIR"
+cp "$SOURCE_DIR"/linux/bin/*.* "$TARGET_DIR/bin"
+echo -e "Easy-Rails files copied successfully.\n"
 
 # Compiling and installing Ruby
-cd "$SOURCE_DIR/linux"
-sudo tar jxvf "$RUBY.tar.bz2"
-cd "$RUBY"
-sudo ./configure --prefix "$TARGET_DIR/ruby/"
-sudo make
-sudo make install
-sudo make install-doc
+echo Extracting Ruby source files...
+cd "$SOURCE_DIR"
+tar xzvf "$DOWNLOAD_DIR/ruby-$RUBY_VERSION.tar.gz"
+echo -e "Ruby source files extracted successfully.\n"
 
-# Running Ruby Console
-cd "$TARGET_DIR"
-./ruby_console.sh
+echo Compiling Ruby...
+cd "ruby-$RUBY_VERSION"
+./configure --prefix="$TARGET_DIR/ruby" --build=i686-linux --enable-install-doc
+make
+echo -e "Ruby compiled successfully.\n"
+
+echo Installing Ruby...
+make install
+echo -e "Ruby installed successfully.\n"
+
+# Install RubyGems
+unzip -q -o "$DOWNLOAD_DIR/rubygems-$RUBYGEMS_VERSION.zip" -d "$SOURCE_DIR"
+cd "$SOURCE_DIR/rubygems-$RUBYGEMS_VERSION"
+ruby setup.rb --prefix="$TARGET_DIR/ruby" --no-format-executable -V --no-ri --no-rdoc
+cd ..
+
+# Update gem script
+sed -i "1s|#!$TARGET_DIR/ruby/bin/ruby|#!/usr/bin/env ruby|g" "$TARGET_DIR/ruby/bin/gem"
+
+# Update RubyGems and sources
+gem update -V --system --no-ri --no-rdoc
+gem sources -V -a http://gems.github.com
+
+# Install gems
+gem install rails   -v $RAILS_VERSION -V
+gem install mongrel -V
+# Database
+gem install mysql -V
+# gem install pg -V # Main postgresql gem. written in C.
+gem install postgres-pr -V # Accessing PostgreSQL from pure Ruby
+gem install sqlite3-ruby -V # sqlite3-ruby requires libsqlite3-dev (sudo apt-get install libsqlite3-dev)
+# Pagination
+# gem install will_paginate -V --no-ri --no-rdoc
+# Authentication
+# gem install devise -V --no-ri --no-rdoc
+# gem install authlogic -V --no-ri --no-rdoc
+# Authorization
+# gem install cancan -V --no-ri --no-rdoc
+# gem install declarative_authorization -V --no-ri --no-rdoc
+# Upload management
+# gem install paperclip -V --no-ri --no-rdoc
+# Calendar date picker
+# gem install calendar_date_select -V --no-ri --no-rdoc
+# Test
+# gem install rspec-rails -V --no-ri --no-rdoc
+# gem install cucumber-rails -V --no-ri --no-rdoc
+# gem install metric_fu -V --no-ri --no-rdoc
+# gem install webrat -V --no-ri --no-rdoc
+# Debug
+# gem install rails-footnotes -V --no-ri --no-rdoc
+# gem install ruby-debug -V --no-ri --no-rdoc
+# Code smells
+# gem install reek -V --no-ri --no-rdoc
+# Admin
+# gem install rails-admin -V --no-ri --no-rdoc
+# Forms
+# gem install simple_form -V --no-ri --no-rdoc
+# responders to dry up your Rails 3 app
+# gem install responders -V --no-ri --no-rdoc
+
+# Remove old gems
+gem cleanup -q -V
+
+# Update ruby scripts
+find "$TARGET_DIR/ruby/bin" -type f -exec sed -i "1s|#!$TARGET_DIR/ruby/bin/ruby|#!/usr/bin/env ruby|g" {} \;
+
+# SQLite files
+unzip -q -o "$DOWNLOAD_DIR/sqlite-shell-linux-x86-$SQLITE_VERSION.zip" -d "$TARGET_DIR/sqlite"
+echo -e "SQLite files extracted successfully.\n"
+
+# Change easy-rails scripts execution mode
+chmod a+x "$TARGET_DIR/ruby_console.sh" "$TARGET_DIR"/bin/*
 
